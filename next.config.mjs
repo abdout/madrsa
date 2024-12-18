e54671b61bbf913cbed9c1ccb526f22be142ec4b
Databayt <osmanabdout@hotmail.com>
2024-12-18 17:05:20 +0300

    x

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
