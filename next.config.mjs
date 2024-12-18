4109e1074dbef5210bc60e6a79e957d2bcdb3d7e
Databayt <osmanabdout@hotmail.com>
2024-12-18 11:33:38 +0300

    x

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.pexels.com" }],
   },
+  reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias, // Important: keep existing aliases
+      "@clerk/elements": path.resolve(__dirname, "node_modules/@clerk/elements"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
